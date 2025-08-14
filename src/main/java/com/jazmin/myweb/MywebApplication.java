c0b46c0c133d983de130153562cebc532dae32f3
Jazzy67q12 <142210366@utsoe.edu.mx}>
2025-08-13 22:20:42 -0600

    host en render

---
+++ src/main/java/com/jazmin/myweb/MywebApplication.java
@@ -0,0 +1,19 @@
+package com.jazmin.myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class MywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebApplication.class, args);
+	}
+
+@GetMapping("/")
+	public String home(){
+		return"index";
+	}
+
+}
